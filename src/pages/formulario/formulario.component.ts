dabb93729bd61fcb5f5b79eeb3858d91324a3dcd
oscar <oscarsmbaquero@gmail.com>
2024-09-02 19:27:23 +0200

    primeros calculos

--- src/pages/formulario/formulario.component.ts
+++ src/pages/formulario/formulario.component.ts
@@ -18,9 +18,12 @@ import { aprovisionamiento } from '../../app/utils/mocks/aprovisionamiento';
 export class FormularioComponent implements OnInit {
   sostenibilidadForm!: FormGroup;
 
+  totalScore= 0;
+
   aprovisionamiento = aprovisionamiento;
   // Objeto para almacenar colores basados en el nivel seleccionado
   labelBackgrounds: { [key: string]: string } = {};
+  fieldScores: { [key: string]: number } = {};
 
   
   
@@ -49,12 +52,69 @@ export class FormularioComponent implements OnInit {
     * @param name 
     * @param value 
     */
-   onDropdownChange(name: string, value: any): void {
-    console.log('Campo:', name, 'Valor seleccionado:', value);
-    switch(value.value) {
+  //  onDropdownChange(name: string, value: any): void {
+  //   console.log('Campo:', name, 'Valor seleccionado:', value);
+
+  //   const selectedItem = this.aprovisionamiento.find(item => item.name === name);
+  //   console.log(selectedItem);
+  //   if (selectedItem) {
+  //     // Buscar la opción seleccionada dentro del item
+  //     const selectedOption = selectedItem.options.find(option => option.value === value.value);
+  
+  //     if (selectedOption) {
+  //       // Sumar el puntaje de la opción seleccionada al puntaje total
+  //       this.totalScore += selectedOption.score;
+  //       console.log('Puntaje total:', this.totalScore);
+  //     }
+  //   }
+    
+
+
+  //   switch(value.value) {
+  //     case 'nivel1':
+  //       console.log(value,'asasasa');        
+  //       this.labelBackgrounds[name] = 'red';
+  //       break;
+  //     case 'nivel2':
+  //       this.labelBackgrounds[name] = 'orange';
+  //       break;
+  //     case 'nivel3':
+  //       this.labelBackgrounds[name] = 'orange';
+  //       break;
+  //     case 'nivel4':
+  //       this.labelBackgrounds[name] = 'lightgreen';
+  //       break;
+  //     case 'nivel5':
+  //       this.labelBackgrounds[name] = 'green';
+  //       break;
+  //     default:        
+  //       this.labelBackgrounds[name] = 'black'; // Color por defecto
+  //   }
+  // }
+  onDropdownChange(name: string, value: any): void {
+    console.log('Campo:', name, 'Valor seleccionado:', value);  
+    // Buscar el item correspondiente por el nombre
+    const selectedItem = this.aprovisionamiento.find(item => item.name === name);  
+    if (selectedItem) {
+      // Buscar la opción seleccionada dentro del item
+      const selectedOption = selectedItem.options.find(option => option.value === value.value);
+  
+      if (selectedOption) {
+        // Obtener el puntaje de la opción seleccionada
+        const newScore = selectedOption.score || 0;  
+        // Obtener el puntaje anterior del campo, si existe
+        const oldScore = this.fieldScores[name] || 0;  
+        // Ajustar el puntaje total
+        this.totalScore += newScore - oldScore;  
+        // Actualizar el puntaje del campo en fieldScores
+        this.fieldScores[name] = newScore;  
+        console.log('Puntaje total:', this.totalScore);
+      }
+    }
+  
+    // Cambiar el color del fondo según el valor seleccionado
+    switch (value.value) {
       case 'nivel1':
-        console.log(value,'asasasa');
-        
         this.labelBackgrounds[name] = 'red';
         break;
       case 'nivel2':
@@ -69,7 +129,7 @@ export class FormularioComponent implements OnInit {
       case 'nivel5':
         this.labelBackgrounds[name] = 'green';
         break;
-      default:        
+      default:
         this.labelBackgrounds[name] = 'black'; // Color por defecto
     }
   }
