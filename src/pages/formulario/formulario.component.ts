2524884757785e8689e90dcac7fc4a2f7c94c864
oscar <oscarsmbaquero@gmail.com>
2024-09-02 18:48:40 +0200

    prueba primer input por color

--- src/pages/formulario/formulario.component.ts
+++ src/pages/formulario/formulario.component.ts
@@ -19,147 +19,59 @@ export class FormularioComponent implements OnInit {
   sostenibilidadForm!: FormGroup;
 
   aprovisionamiento = aprovisionamiento;
-  //   {
-  //     name: 'campo1_b1',
-  //     label: '1. CERTIFICADO DE PRODUCCIÓN ECOLÓGICA',
-  //     labelsecond: '¿Hay alimentos de producción ecologica en el menú?',
-  //     options: [
-  //       { label: 'Ningún pescado utilizado en el menú está asociado a algún certificado de calidad', value: 'nivel1' },
-  //       { label: 'Al menos el 5% de los pescados servidos en el menú tienen algún certificado de calidad', value: 'nivel2' },
-  //       { label: 'Al menos el 20% de los pescados del menú tienen algún certificado de calidad', value: 'nivel3' },
-  //       { label: 'Al menos el 35% de los pescados del menú tienen algún certificado de calidad', value: 'nivel4' },
-  //       { label: 'Más del 50% del pescado utilizado en el menú está asociado a algún certificado de calidad', value: 'nivel5' }
-  //     ]
-  //   },
-  //   {
-  //     name: 'campo2_b1',
-  //     label: '2. COMPROMISO CON LA GANADERIA EXTENSIVA',
-  //     labelsecond: '¿Los alimentos de origen animal proceden de ganadería extensiva?',
-  //     options: [
-  //       { label: 'Ningún producto utilizado en el menú proviene del comercio justo o de alguna iniciativa cooperativa', value: 'nivel1' },
-  //       { label: 'Al menos el 5% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel2' },
-  //       { label: 'Al menos el 20% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel3' },
-  //       { label: 'Al menos el 35% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel4' },
-  //       { label: 'Más del 50% de los productos utilizados en el menú provienen del comercio justo o de alguna iniciativa cooperativa', value: 'nivel5' }
-  //     ]
-  //   },
-  //   {
-  //     name: 'campo2_b1',
-  //     label: '3. CERTIFICADO DE PESCA SOSTENIBLE ',
-  //     labelsecond: '¿EL pescado procede de pesca sostenible?',
-  //     options: [
-  //       { label: 'Ningún producto utilizado en el menú proviene del comercio justo o de alguna iniciativa cooperativa', value: 'nivel1' },
-  //       { label: 'Al menos el 5% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel2' },
-  //       { label: 'Al menos el 20% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel3' },
-  //       { label: 'Al menos el 35% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel4' },
-  //       { label: 'Más del 50% de los productos utilizados en el menú provienen del comercio justo o de alguna iniciativa cooperativa', value: 'nivel5' }
-  //     ]
-  //   },
-  //   {
-  //     name: 'campo2_b1',
-  //     label: '4. PRODUCTOS DE COMERCIO JUSTO / INICIATIVA COOPERATIVA  ',
-  //     labelsecond: '¿Consumís productos de comercio justo o de iniciativas de economía cooperativa?',
-  //     options: [
-  //       { label: 'Ningún producto utilizado en el menú proviene del comercio justo o de alguna iniciativa cooperativa', value: 'nivel1' },
-  //       { label: 'Al menos el 5% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel2' },
-  //       { label: 'Al menos el 20% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel3' },
-  //       { label: 'Al menos el 35% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel4' },
-  //       { label: 'Más del 50% de los productos utilizados en el menú provienen del comercio justo o de alguna iniciativa cooperativa', value: 'nivel5' }
-  //     ]
-  //   },
-  //   {
-  //     name: 'campo2_b1',
-  //     label: '5. DENOMINACIONES DOP/IGP   ',
-  //     labelsecond: '¿Consumís productos de comercio justo o de iniciativas de economía cooperativa?',
-  //     options: [
-  //       { label: 'Ningún producto utilizado en el menú proviene del comercio justo o de alguna iniciativa cooperativa', value: 'nivel1' },
-  //       { label: 'Al menos el 5% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel2' },
-  //       { label: 'Al menos el 20% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel3' },
-  //       { label: 'Al menos el 35% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel4' },
-  //       { label: 'Más del 50% de los productos utilizados en el menú provienen del comercio justo o de alguna iniciativa cooperativa', value: 'nivel5' }
-  //     ]
-  //   },
-  //   {
-  //     name: 'campo2_b1',
-  //     label: '6. TERRITORIALIDAD DE LOS ALIMENTOS',
-  //     labelsecond: '¿Dónde se producen los alimentos que compráis?',
-  //     options: [
-  //       { label: 'Ningún producto utilizado en el menú proviene del comercio justo o de alguna iniciativa cooperativa', value: 'nivel1' },
-  //       { label: 'Al menos el 5% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel2' },
-  //       { label: 'Al menos el 20% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel3' },
-  //       { label: 'Al menos el 35% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel4' },
-  //       { label: 'Más del 50% de los productos utilizados en el menú provienen del comercio justo o de alguna iniciativa cooperativa', value: 'nivel5' }
-  //     ]
-  //   },
-  //   {
-  //     name: 'campo2_b1',
-  //     label: '7. ESTACIONALIDAD DEL ALIMENTO ',
-  //     labelsecond: '¿Consumís alimentos de temporada?',
-  //     options: [
-  //       { label: 'Ningún producto utilizado en el menú proviene del comercio justo o de alguna iniciativa cooperativa', value: 'nivel1' },
-  //       { label: 'Al menos el 5% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel2' },
-  //       { label: 'Al menos el 20% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel3' },
-  //       { label: 'Al menos el 35% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel4' },
-  //       { label: 'Más del 50% de los productos utilizados en el menú provienen del comercio justo o de alguna iniciativa cooperativa', value: 'nivel5' }
-  //     ]
-  //   },
-  //   {
-  //     name: 'campo2_b1',
-  //     label: '8. FRESCURA DE LOS ALIMENTOS    ',
-  //     labelsecond: '¿Consumís alimentos frescos?',
-  //     options: [
-  //       { label: 'Ningún producto utilizado en el menú proviene del comercio justo o de alguna iniciativa cooperativa', value: 'nivel1' },
-  //       { label: 'Al menos el 5% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel2' },
-  //       { label: 'Al menos el 20% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel3' },
-  //       { label: 'Al menos el 35% de los productos del menú provienen de comercio justo o iniciativa cooperativa', value: 'nivel4' },
-  //       { label: 'Más del 50% de los productos utilizados en el menú provienen del comercio justo o de alguna iniciativa cooperativa', value: 'nivel5' }
-  //     ]
-  //   },
-  //   // Agrega los otros campos de manera similar
-  // ];
+  // Objeto para almacenar colores basados en el nivel seleccionado
+  labelBackgrounds: { [key: string]: string } = {};
 
-  indicadoresBloque2 = [
-    { name: 'campo1_b2', label: '9. CANTIDAD DE ENVASES Y EMBALAJES ', labelsecond:'' ,options: this.createOptions() },
-    { name: 'campo2_b2', label: '10. PROPORCIÓN Y TIPO DE P LÁSTICOS UTILIZADOS ', labelsecond:'' ,options: this.createOptions() },
-    { name: 'campo3_b2', label: '11. PROPORCIÓN DE EMBALAJES ALUMÍNICOS Y ENLATADOS ', labelsecond:'' ,options: this.createOptions() },
-    { name: 'campo5_b2', label: '12. ENERGÍA UTILIZADA POR LOS VEHÍCULOS IMPLICADOS ', labelsecond:'' ,options: this.createOptions() },
-    { name: 'campo6_b2', label: '13. KILOMETRAJE RECORRIDO ', labelsecond:'' ,options: this.createOptions() },
-  ];
-
-  indicadoresBloque3 = [
-    { name: 'campo1_b3', label: '14. ENERGÍA UTILIZADA EN COCINADO Y CONSERVACIÓN ', options: this.createOptions() },
-    { name: 'campo2_b3', label: 'Campo 2 Bloque 3', options: this.createOptions() },
-    { name: 'campo3_b3', label: 'Campo 3 Bloque 3', options: this.createOptions() },
-    { name: 'campo4_b3', label: 'Campo 4 Bloque 3', options: this.createOptions() },
-    { name: 'campo5_b3', label: 'Campo 5 Bloque 3', options: this.createOptions() },
-    { name: 'campo6_b3', label: 'Campo 6 Bloque 3', options: this.createOptions() },
-    { name: 'campo7_b3', label: 'Campo 7 Bloque 3', options: this.createOptions() },
-    { name: 'campo8_b3', label: 'Campo 8 Bloque 3', options: this.createOptions() },
-  ];
+  
+  
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.sostenibilidadForm = this.fb.group({});
     this.addControls(this.aprovisionamiento);
-    this.addControls(this.indicadoresBloque2);
-    this.addControls(this.indicadoresBloque3);
+    this.aprovisionamiento.forEach(item => {
+      this.sostenibilidadForm.get(item.name)?.valueChanges.subscribe(value => {
+        this.onDropdownChange(item.name, value);
+      });
+    });
   }
 
-  createOptions() {
-    return [
-      { label: 'Nivel 5', value: 5 },
-      { label: 'Nivel 4', value: 4 },
-      { label: 'Nivel 3', value: 3 },
-      { label: 'Nivel 2', value: 2 },
-      { label: 'Nivel 1', value: 1 }
-    ];
-  }
+  
 
   addControls(indicadores: any) {
     indicadores.forEach((indicador:any) => {
       this.sostenibilidadForm.addControl(indicador.name, this.fb.control('', Validators.required));
     });
+  }
+   /**
+    * Método para manejar los cambios
+    * @param name 
+    * @param value 
+    */
+   onDropdownChange(name: string, value: any): void {
+    console.log('Campo:', name, 'Valor seleccionado:', value);
+    switch(value.value) {
+      case 'nivel1':
+        console.log(value,'asasasa');
+        
+        this.labelBackgrounds[name] = 'red';
+        break;
+      case 'nivel2':
+        this.labelBackgrounds[name] = 'orange';
+        break;
+      case 'nivel3':
+        this.labelBackgrounds[name] = 'orange';
+        break;
+      case 'nivel4':
+        this.labelBackgrounds[name] = 'lightgreen';
+        break;
+      case 'nivel5':
+        this.labelBackgrounds[name] = 'green';
+        break;
+      default:        
+        this.labelBackgrounds[name] = 'black'; // Color por defecto
+    }
   }
 
   onSubmit(): void {
