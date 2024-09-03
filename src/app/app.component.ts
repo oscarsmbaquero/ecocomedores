9f0793ef0bbf365e3e39f9aaff8ddb05d0515ba4
oscar <oscarsmbaquero@gmail.com>
2024-09-03 19:16:16 +0200

    boton up ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../core/components/header/header.component";
 import { FooterComponent } from "../core/components/footer/footer.component";
 import { LogoComponent } from '../shared/components/logo/logo.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LogoComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LogoComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecocomedores';
+
+  scrollPosition = 0;
+  
+   // Método para manejar el evento de scroll
+   @HostListener('window:scroll', [])
+   onWindowScroll() {
+     this.scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+     console.log(this.scrollPosition);
+   }
+  
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    
+    
+  }
+
 }
